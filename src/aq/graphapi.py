9b279ad3ad16740924660172caa325a6f05c882d
John Julien <john.julien.qso5@statefarm.com>
2019-10-31 14:04:20 -0500

    Added basic passthrough of Graph API GET calls

---
+++ src/aq/graphapi.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+class AzureGraphAPI():
+
+    BASE_GRAPH_URL = 'https://graph.microsoft.com/v1.0'
+
+    def __init__(self, token):
+        self.token = token
+    
+    def get_headers(self):
+        headers = {
+            "Authorization": f"Bearer {self.token.raw_token}"
+        }
+        return headers
+    
+    def get(self, path):
+        full_url = f"{AzureGraphAPI.BASE_GRAPH_URL}/{path}"
+        response = requests.get(full_url, headers=self.get_headers())
+        return response.text
