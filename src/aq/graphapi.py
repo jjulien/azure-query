d723321356cd404f21cb326577dfeea880f5040a
John Julien <john.julien.qso5@statefarm.com>
2019-11-19 11:00:10 -0600

    Enable the use of the beta API

--- src/aq/graphapi.py
+++ src/aq/graphapi.py
@@ -4,10 +4,15 @@ import json
 class AzureGraphAPI():
 
     BASE_GRAPH_URL = 'https://graph.microsoft.com/v1.0'
+    BETA_GRAPH_URL = 'https://graph.microsoft.com/beta'
 
-    def __init__(self, token):
+    def __init__(self, token, beta=False):
         self.token = token
-    
+        if beta:
+            self.base_url = AzureGraphAPI.BETA_GRAPH_URL
+        else:
+            self.base_url = AzureGraphAPI.BASE_GRAPH_URL
+
     def get_headers(self):
         headers = {
             "Authorization": f"Bearer {self.token.raw_token}"
@@ -15,6 +20,6 @@ class AzureGraphAPI():
         return headers
     
     def get(self, path, query_params={}):
-        full_url = f"{AzureGraphAPI.BASE_GRAPH_URL}/{path}"
+        full_url = f"{self.base_url}/{path}"
         response = requests.get(full_url, headers=self.get_headers(), params=query_params)
         return json.loads(response.text)
